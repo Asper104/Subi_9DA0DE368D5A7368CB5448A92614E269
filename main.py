f9c96a643e534177bbbaaf32ee2e2fa0b29a1d0f
Asper104 <144360296+Asper104@users.noreply.github.com>
2023-09-08 12:14:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+def fact(n):
+  """this is a recursive function to find the factorial of an integer"""
+  if n==0 or n==1:
+    return 1
+
+  else:
+       return (n*fact(n-1))
+
+number=int(input("enter the number:"))
+x=fact(number)
+print("the factorial of {} is {} "
+. format (number,x))
